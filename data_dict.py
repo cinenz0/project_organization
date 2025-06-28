5493b1e10098cf4ecb7598b7c92ef25305a48d70
Lorenzo Mancinelli <lorenzomancinelli05@gmail.com>
2025-06-28 19:07:45 -0300

    Finished data_dict.py

--- data_dict.py
+++ data_dict.py
@@ -1,12 +1,35 @@
-
-
 data_dict = [
     {
         'Folder':'PDF',
         'Suffixes':['.pdf'],
     },
     {
+        'Folder':'Excel',
+        'Suffixes':['.csv','.xlsx', 'xls'],
+    },
+
+    {
+        'Folder':'PowerPoint',
+        'Suffixes':['.ppt','.pptx'],
+    },
+
+    {
         'Folder':'Word',
         'Suffixes':['.docx','.doc'],
+    },
+
+    {
+        'Folder':'Images',
+        'Suffixes':['.png','.jpg', '.jpeg', '.gif','.ico'],
+    },
+
+    {
+        'Folder':'Video',
+        'Suffixes':['.mp4','.mov', 'wmv'],
+    },
+
+    {
+        'Folder':'Audio',
+        'Suffixes':['.mp3','.wav'],
     }
 ]
